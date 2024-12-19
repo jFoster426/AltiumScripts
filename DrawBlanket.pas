f5cbecae79f354bceb74a4c9cc7188add380576a
Judd Foster <judd.foster2@gmail.com>
2024-12-18 20:55:54 -0800

    update drawblanket to switch between blanket and polyline

--- DrawBlanket.pas
+++ DrawBlanket.pas
@@ -1,34 +1,12 @@
-Procedure ConvertPolylineToBlanket;
 Var
     SchDoc : ISch_Document;
     Blanket : ISch_Blanket;
-
-    LineIterator : ISch_Iterator;
-    Line : ISch_Polyline; // was previously ISch_Line
+    Line : ISch_Polyline;
     i : Integer;
+    Iterator : ISch_Iterator;
 
+Procedure ConvertPolylineToBlanket;
 Begin
-    // Initialize the robots in Schematic editor.
-    If SchServer = Nil Then Exit;
-    SchDoc := SchServer.GetCurrentSchDocument;
-    If SchDoc = Nil Then Exit;
-
-    LineIterator := SchDoc.SchIterator_Create;
-    If LineIterator = Nil Then Exit;
-    LineIterator.AddFilter_ObjectSet(MkSet(ePolyline));
-    Line := LineIterator.FirstSchObject;
-    While Line <> Nil Do
-    Begin
-        If Line.Selection = True Then
-        Begin
-            Break;
-        End;
-        Line := LineIterator.NextSchObject;
-    End;
-    SchDoc.SchIterator_Destroy(LineIterator);
-
-    If Line.Selection = False Then Exit;
-
     SchServer.ProcessControl.PreProcess(SchDoc, '');
     Blanket := SCHServer.SchObjectFactory(eBlanket, eCreate_Default);
 
@@ -54,36 +32,7 @@ Begin
 End;
 
 Procedure ConvertBlanketToPolyline;
-Var
-    SchDoc : ISch_Document;
-    Line : ISch_Polyline;
-
-    BlanketIterator : ISch_Iterator;
-    Blanket : ISch_Blanket;
-    i : Integer;
-
 Begin
-    // Initialize the robots in Schematic editor.
-    If SchServer = Nil Then Exit;
-    SchDoc := SchServer.GetCurrentSchDocument;
-    If SchDoc = Nil Then Exit;
-
-    BlanketIterator := SchDoc.SchIterator_Create;
-    If BlanketIterator = Nil Then Exit;
-    BlanketIterator.AddFilter_ObjectSet(MkSet(eBlanket));
-    Blanket := BlanketIterator.FirstSchObject;
-    While Blanket <> Nil Do
-    Begin
-        If Blanket.Selection = True Then
-        Begin
-            Break;
-        End;
-        Blanket := BlanketIterator.NextSchObject;
-    End;
-    SchDoc.SchIterator_Destroy(BlanketIterator);
-
-    If Blanket.Selection = False Then Exit;
-
     SchServer.ProcessControl.PreProcess(SchDoc, '');
     Line := SCHServer.SchObjectFactory(ePolyline, eCreate_Default);
 
@@ -92,7 +41,7 @@ Begin
         Line.InsertVertex(i);
         Line.Vertex[i] := Blanket.Vertex[i];
     End;
-    Line.Color := $0000FF;
+    Line.Color := $FF0000;
     Line.LineWidth := 1;
     Line.LineStyle := eLineStyleDashed;
 
@@ -104,4 +53,49 @@ Begin
     SchDoc.RemoveSchObject(Blanket);
     SchServer.RobotManager.SendMessage(SchDoc.I_ObjectAddress, c_BroadCast, SCHM_PrimitiveRegistration, Blanket.I_ObjectAddress);
     SchServer.ProcessControl.PostProcess(SchDoc, '');
+End;
+
+Procedure Main;
+Begin
+    // Is line selected
+    If SchServer = Nil Then Exit;
+    SchDoc := SchServer.GetCurrentSchDocument;
+    If SchDoc = Nil Then Exit;
+
+    Iterator := SchDoc.SchIterator_Create;
+    If Iterator = Nil Then Exit;
+    Iterator.AddFilter_ObjectSet(MkSet(ePolyline));
+    Line := Iterator.FirstSchObject;
+    While Line <> Nil Do
+    Begin
+        If Line.Selection = True Then
+        Begin
+            ConvertPolylineToBlanket();
+            SchDoc.SchIterator_Destroy(Iterator);
+            Exit;
+        End;
+        Line := Iterator.NextSchObject;
+    End;
+    SchDoc.SchIterator_Destroy(Iterator);
+
+    // Is blanket selected
+    If SchServer = Nil Then Exit;
+    SchDoc := SchServer.GetCurrentSchDocument;
+    If SchDoc = Nil Then Exit;
+
+    Iterator := SchDoc.SchIterator_Create;
+    If Iterator = Nil Then Exit;
+    Iterator.AddFilter_ObjectSet(MkSet(eBlanket));
+    Blanket := Iterator.FirstSchObject;
+    While Blanket <> Nil Do
+    Begin
+        If Blanket.Selection = True Then
+        Begin
+            ConvertBlanketToPolyline();
+            SchDoc.SchIterator_Destroy(Iterator);
+            Exit;
+        End;
+        Blanket := Iterator.NextSchObject;
+    End;
+    SchDoc.SchIterator_Destroy(Iterator);
 End;
