bd8eb9f44c4facd59022b5fda0825bb5d0dfa8ef
Judd Foster <judd.foster2@gmail.com>
2025-05-31 08:43:18 -0700

    add samewidth2.pas

--- AutomatePolygon.pas
+++ AutomatePolygon.pas
@@ -1,112 +1,150 @@
 var
-   Board : IPCB_Board;
-   Object1 : IPCB_Primitive;
-   CountObjects : Integer;
-   index1 : Integer;
-   layerName : String;
+    board          : IPCB_Board;
+    stack          : IPCB_LayerStack;
+    lyrObj         : IPCB_LayerObject;
+    object1        : IPCB_Primitive;
+    layerIterator  : IPCB_LayerIterator;
+    countObjects   : Integer;
+    index1         : Integer;
+    layerName      : String;
+
+function getLayerIDFromShortLayerName(shortLayerName : String) : TV6_Layer;
+begin
+    if board = nil then exit;
+    stack := board.LayerStack;
+    // get first layer of the class type.
+    lyrObj := stack.First(eLayerClass_Signal);
+    // exit if layer type is not available in stack
+    if lyrObj = nil then exit;
+    // iterate through layers and display each layer name
+    repeat
+        if lyrObj.Name = shortLayerName then
+        begin
+            result := lyrObj.V6_LayerID;
+            exit;
+        end;
+        lyrObj := stack.Next(eLayerClass_Signal, lyrObj);
+    until lyrObj = nil;
+end;
 
 function changeLayer(obj : IPCB_Primitive);
 begin
-    layerName := Layer2String(Object1.Layer);
+    layerName := Layer2String(object1.Layer);
     case layerName of
         'Mechanical Layer 101':
             begin
                 // Code for OL1
-                Object1.Layer := String2Layer('Top Layer');
+                object1.Layer := getLayerIDFromShortLayerName('L1');
             end;
         'Mechanical Layer 102':
             begin
                 // Code for OL2
-                Object1.Layer := String2Layer('Mid Layer 1');
+                object1.Layer := getLayerIDFromShortLayerName('L2');
             end;
         'Mechanical Layer 103':
             begin
                 // Code for OL3
-                Object1.Layer := String2Layer('Mid Layer 2');
+                object1.Layer := getLayerIDFromShortLayerName('L3');
             end;
         'Mechanical Layer 104':
             begin
                 // Code for OL4
-                Object1.Layer := String2Layer('Mid Layer 3');
+                object1.Layer := getLayerIDFromShortLayerName('L4');
             end;
         'Mechanical Layer 105':
             begin
                 // Code for OL5
-                Object1.Layer := String2Layer('Mid Layer 4');
+                object1.Layer := getLayerIDFromShortLayerName('L5');
             end;
         'Mechanical Layer 106':
             begin
                 // Code for OL6
-                Object1.Layer := String2Layer('Mid Layer 5');
+                object1.Layer := getLayerIDFromShortLayerName('L6');
             end;
         'Mechanical Layer 107':
             begin
                 // Code for OL7
-                Object1.Layer := String2Layer('Mid Layer 6');
+                object1.Layer := getLayerIDFromShortLayerName('L7');
             end;
         'Mechanical Layer 108':
             begin
                 // Code for OL8
-                Object1.Layer := String2Layer('Mid Layer 7');
+                object1.Layer := getLayerIDFromShortLayerName('L8');
             end;
         'Mechanical Layer 109':
             begin
                 // Code for OL9
-                Object1.Layer := String2Layer('Mid Layer 8');
+                object1.Layer := getLayerIDFromShortLayerName('L9');
             end;
         'Mechanical Layer 110':
             begin
                 // Code for OL10
-                Object1.Layer := String2Layer('Bottom Layer');
+                object1.Layer := getLayerIDFromShortLayerName('L10');
+            end;
+        'Mechanical Layer 111':
+            begin
+                // Code for OL11
+                object1.Layer := getLayerIDFromShortLayerName('L11');
+            end;
+        'Mechanical Layer 112':
+            begin
+                // Code for OL12
+                object1.Layer := getLayerIDFromShortLayerName('L12');
+            end;
+        'Mechanical Layer 113':
+            begin
+                // Code for OL13
+                object1.Layer := getLayerIDFromShortLayerName('L13');
+            end;
+        'Mechanical Layer 114':
+            begin
+                // Code for OL14
+                object1.Layer := getLayerIDFromShortLayerName('L14');
             end;
     end;
 end;
 
-
 procedure Main;
 begin
-    Board := PCBServer.GetCurrentPCBBoard;
+    board := PCBServer.GetCurrentPCBBoard;
+    If board = Nil Then Exit;
 
-    If Board = Nil Then Exit;
-
-    //PCBServer.PreProcess;
-    
     // Get the selected tracks/arcs
     // Improves speed over the old script
-    CountObjects := Board.SelectecObjectCount;
+    countObjects := board.SelectecObjectCount;
 
     for index1 := 0 to CountObjects-1 do begin
-        Object1 := Board.SelectecObject(index1);
+        object1 := board.SelectecObject(index1);
 
-        if (Object1.ObjectId = ePolyObject) then
+        if (object1.ObjectId = ePolyObject) then
         begin
-            Object1.BeginModify;
-            Object1.SetState_PolyHatchStyle(ePolySolid);
-            Object1.SetState_ArcApproximation(MMsToCoord(0.001));
-            Object1.SetState_PourOver(true);
-            Object1.SetState_RemoveDead(true);
-            Object1.SetState_RemoveNarrowNecks(true);
-            Object1.SetState_NeckWidthThreshold(MMsToCoord(0.1));
-            Object1.Rebuild();
-            changeLayer(Object1);
-            Object1.EndModify;
-            Object1.GraphicallyInvalidate;
+            object1.BeginModify;
+            object1.SetState_PolyHatchStyle(ePolySolid);
+            object1.SetState_ArcApproximation(MMsToCoord(0.001));
+            object1.SetState_PourOver(true);
+            object1.SetState_RemoveDead(true);
+            object1.SetState_RemoveNarrowNecks(true);
+            object1.SetState_NeckWidthThreshold(MMsToCoord(0.1));
+            object1.Rebuild();
+            changeLayer(object1);
+            object1.EndModify;
+            object1.GraphicallyInvalidate;
         end;
 
-        if (Object1.ObjectId = eRegionObject) then
+        if (object1.ObjectId = eRegionObject) then
         begin
-            Object1.BeginModify;
-            Object1.SetState_ArcApproximation(MMsToCoord(0.001));
-            changeLayer(Object1);
-            Object1.EndModify;
-            Object1.GraphicallyInvalidate;
+            object1.BeginModify;
+            object1.SetState_ArcApproximation(MMsToCoord(0.001));
+            changeLayer(object1);
+            object1.EndModify;
+            object1.GraphicallyInvalidate;
         end;
     end;
 
-    Board.CurrentLayer := Object1.Layer;
-    Board.ViewManager_UpdateLayerTabs;
+    board.CurrentLayer := object1.Layer;
+    board.ViewManager_UpdateLayerTabs;
 
     //PCBServer.PostProcess;
-    Board.ViewManager_FullUpdate;
+    board.ViewManager_FullUpdate;
     Client.SendMessage('PCB:Zoom', 'Action=Redraw', 255, Client.CurrentView);
 end;
---
+++ SameWidth2.pas
@@ -0,0 +1,145 @@
+Var
+    Board : IPCB_Board;
+    CoordTol : Integer;
+
+Function CoordMatch(X1 : TCoord, Y1 : TCoord, X2 : TCoord, Y2 : TCoord) : Boolean;
+Begin
+    Result := False;
+    If sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1))) < CoordTol Then
+    Begin
+        Result := True;
+    End;
+End;
+
+
+Procedure GetNextPrimitive(Primitive : IPCB_Primitive, x : Integer, y : Integer, w : Integer);
+Var
+    Iterator : IPCB_SpatialIterator;
+    SetOfLayers : IPCB_LayerSet;
+    PrimWidth : Integer;
+    NewPrimWidth : Integer;
+    SpX, SpY : Integer;
+    NewPrimitive : IPCB_Object;
+    Coord1Match, Coord2Match : Boolean;
+Begin
+    // Initialize varibles
+    Coord1Match := False;
+    Coord2Match := False;
+
+    // Create SpatialIterator based on original primitive's layer,
+    // only select tracks and arcs
+    Iterator := Board.SpatialIterator_Create;
+    SetOfLayers := LayerSet.CreateLayerSet;
+    SetOfLayers.Include(Primitive.Layer);
+    Iterator.AddFilter_IPCB_LayerSet(SetOfLayers);
+    Iterator.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
+
+    // The values in (x, y) point to the previous location of the SpatialIterator
+    // so create the new SpatialIterator based on the primitive's other coordinate
+    If Primitive.ObjectID = eTrackObject Then
+    Begin
+
+        PrimWidth := Primitive.Width;
+        // Add a filter area for the other vertice
+        If CoordMatch(Primitive.x1, Primitive.y1, x, y) Then
+        Begin
+            SpX := Primitive.x2;
+            SpY := Primitive.y2;
+        End Else
+        If CoordMatch(Primitive.x2, Primitive.y2, x, y) Then
+        Begin
+            SpX := Primitive.x1;
+            SpY := Primitive.y1;
+        End Else
+        Begin
+            // The primitive does not connect to the previous primitive's verticies in any location
+            Exit;
+        End;
+    End Else
+    If Primitive.ObjectID = eArcObject Then
+    Begin
+        PrimWidth := Primitive.LineWidth;
+        // Add a filter area for the other vertice
+        If CoordMatch(Primitive.StartX, Primitive.StartY, x, y) Then
+        Begin
+            SpX := Primitive.EndX;
+            SpY := Primitive.EndY;
+        End Else
+        If CoordMatch(Primitive.EndX, Primitive.EndY, x, y) Then
+        Begin
+            SpX := Primitive.StartX;
+            SpY := Primitive.StartY;
+        End Else
+        Begin
+            // The primitive does not connect to the previous primitive's verticies in any location
+            Exit;
+        End;
+    End;
+    // Bounding box should be W/2 tolerance in any direction to capture touching tracks
+    Iterator.AddFilter_Area(SpX - (PrimWidth/2), SpY - (PrimWidth/2), SpX + (PrimWidth/2), SpY + (PrimWidth/2));
+
+    // Finally iterate through each object in the SpatialIterator
+    NewPrimitive := Iterator.FirstPCBObject;
+    While NewPrimitive <> 0 Do
+    Begin
+        If NewPrimitive.ObjectID = eTrackObject Then
+        Begin
+            NewPrimWidth := NewPrimitive.Width;
+            Coord1Match := CoordMatch(NewPrimitive.x1, NewPrimitive.y1, SpX, SpY);
+            Coord2Match := CoordMatch(NewPrimitive.x2, NewPrimitive.y2, SpX, SpY);
+        End Else
+        If NewPrimitive.ObjectID = eArcObject Then
+        Begin
+            NewPrimWidth := NewPrimitive.LineWidth;
+            Coord1Match := CoordMatch(NewPrimitive.StartX, NewPrimitive.StartY, SpX, SpY);
+            Coord2Match := CoordMatch(NewPrimitive.EndX, NewPrimitive.EndY, SpX, SpY);
+        End;
+        // Recursive condition stops when previously selected object found, or
+        // if line width does not match, or
+        // if start/end coordinates of new vertice to not match previous vertice
+        If ((NewPrimitive.Selected = False) And (Abs(NewPrimWidth - W) < 10) And (Coord1Match Or Coord2Match)) Then
+        Begin
+            NewPrimitive.Selected := True;
+            If (NewPrimitive.ObjectID = eTrackObject) Then
+            Begin
+                GetNextPrimitive(NewPrimitive, SpX, SpY, NewPrimWidth);
+            End Else
+            If (NewPrimitive.ObjectID = eArcObject) Then
+            Begin
+                GetNextPrimitive(NewPrimitive, SpX, SpY, NewPrimWidth);
+            End;
+        End;
+        NewPrimitive := Iterator.NextPCBObject;
+    End;
+    Board.SpatialIterator_Destroy(Iterator);
+End;
+
+Procedure SameWidth2;
+Var
+    PCBObject : IPCB_Primitive;
+Begin
+    Board := PCBServer.GetCurrentPCBBoard;
+    If Board = Nil Then Exit;
+    If Board.SelectecObjectCount = 0 Then
+    Begin
+        Exit;
+    End;
+    PCBObject := Board.SelectecObject[0];
+    If PCBObject.ObjectID = eTrackObject Then
+    Begin
+        CoordTol := PCBObject.Width / 2;
+        // Run in both directions in case of not closed shape
+        GetNextPrimitive(PCBObject, PCBObject.x1, PCBObject.y1, PCBObject.Width);
+        GetNextPrimitive(PCBObject, PCBObject.x2, PCBObject.y2, PCBObject.Width);
+    End Else
+    If PCBObject.ObjectID = eArcObject Then
+    Begin
+        CoordTol := PCBObject.LineWidth / 2;
+        // Run in both directions in case of not closed shape
+        GetNextPrimitive(PCBObject, PCBObject.StartX, PCBObject.StartY, PCBObject.LineWidth);
+        GetNextPrimitive(PCBObject, PCBObject.EndX, PCBObject.EndY, PCBObject.LineWidth);
+    End;
+
+    Board.ViewManager_FullUpdate;
+    Client.SendMessage('PCB:Zoom', 'Action=Redraw' , 255, Client.CurrentView);
+End;
